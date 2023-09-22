b0c0f3821f7f5d3be290dd4f598e945c671e1c1f
wikonico <caramesgarcianico@gmail.com>
2023-09-22 18:13:51 +0200

    terminado 5

--- src/Components/FormSend.tsx
+++ src/Components/FormSend.tsx
@@ -1,21 +1,20 @@
 import './form.css'
-import { useRef, useState } from 'react'
+import { useState } from 'react'
 import emailjs from 'emailjs-com'
 
 export const Form = () => {
-  const form = useRef()
-  const [nombre, setNombre] = useState()
-  const [email, setEmail] = useState()
-  const [text, setText] = useState()
+  const [nombre, setNombre] = useState('')
+  const [email, setEmail] = useState('')
+  const [text, setText] = useState('')
 
   const sendEmail = e => {
     e.preventDefault()
 
-    emailjs.sendForm('service_04hqz4j', 'template_xo4j1zo', form.current, 'E1tmZ1daF97zo7GiL').then(
+    emailjs.sendForm('service_04hqz4j', 'template_xo4j1zo', e.target, 'E1tmZ1daF97zo7GiL').then(
       result => {
-        setNombre(nombre)
-        setEmail(email)
-        setText(text)
+        setNombre('')
+        setEmail('')
+        setText('')
         console.log(result.text)
         alert('Email send, thanks!!')
       },
@@ -26,7 +25,7 @@ export const Form = () => {
   }
 
   return (
-    <form className="form" ref={form} onSubmit={sendEmail}>
+    <form className="form" onSubmit={sendEmail}>
       <label>Send me an email:</label>
       <input value={nombre} placeholder="Name" type="text" name="user_name" />
 
